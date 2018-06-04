e077bde1bd295e7f325904b1f750fb7c27fc0b52
Stephen Wyatt Bush <stephen.wyatt@gmail.com>
2018-06-04 10:21:03 -0500

    Add Hello component

--- src/stories/index.js
+++ src/stories/index.js
@@ -1,15 +1,20 @@
 import React from 'react';
 
-import { storiesOf } from '@storybook/react';
-import { action } from '@storybook/addon-actions';
-import { linkTo } from '@storybook/addon-links';
+import {storiesOf} from '@storybook/react';
+import {action} from '@storybook/addon-actions';
+import {linkTo} from '@storybook/addon-links';
 
-import { Button, Welcome } from '@storybook/react/demo';
+import {Button, Welcome} from '@storybook/react/demo';
+import Hello from '../components/Hello';
 
-storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
+storiesOf('Welcome', module).add('to Storybook', () => (
+  <Welcome showApp={linkTo('Button')} />
+));
 
 storiesOf('Button', module)
-  .add('with text', () => <Button onClick={action('clicked')}>Hello Button</Button>)
+  .add('with text', () => (
+    <Button onClick={action('clicked')}>Hello Button</Button>
+  ))
   .add('with some emoji', () => (
     <Button onClick={action('clicked')}>
       <span role="img" aria-label="so cool">
@@ -17,3 +22,7 @@ storiesOf('Button', module)
       </span>
     </Button>
   ));
+
+storiesOf('Hello', module)
+  .add('with Stephen', () => <Hello name="Stephen" />)
+  .add('with Marc', () => <Hello name="Marc" />);
